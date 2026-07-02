4957135745b873c9d7c72395da1da4e5472e81d4
Tannu Gupta <guptatannu538@gmail.com>
2026-07-02 13:55:05 +0530

    add model_evaluation.py

---
+++ src/models/model_building.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import pickle
+import yaml
+
+from sklearn.ensemble import GradientBoostingClassifier
+
+def load_params(params_path):
+    params=yaml.safe_load(open(params_path, 'r'))['model_building']
+    return params
+
+def load_data(train_path):
+    train_data=pd.read_csv(train_path)
+    X_train=train_data.iloc[:,0:-1].values
+    y_train=train_data.iloc[:,-1].values
+    return X_train, y_train
+
+def train_model(X_train,y_train, params):
+    clf=GradientBoostingClassifier(n_estimators=params['n_estimators'], learning_rate=params['learning_rate'])
+    clf.fit(X_train, y_train)
+    return clf
+
+
+def save_model(clf, model_path="emotion-detection/models/model.pkl"):
+    pickle.dump(clf, open(model_path, "wb"))
+
+
+def main():
+    params=load_params('params.yaml')
+    
+    X_train, y_train=load_data('./emotion-detection/data/processed/train_bow.csv')
+    clf=train_model(X_train, y_train,params)
+    save_model(clf)
+
+
+if __name__=='__main__':
+    main()
---
+++ src/models/model_evaluation.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+
+import pickle
+import json
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score, recall_score, roc_auc_score
+
+def load_model(model_path):
+    clf=pickle.load(open(model_path, 'rb'))
+    return clf
+
+def load_data(data_path):
+    test_data=pd.read_csv(data_path)
+    X_test=test_data.iloc[:,0:-1].values
+    y_test=test_data.iloc[:,-1].values
+    return X_test, y_test
+
+
+
+def make_prediction(clf,X_test):
+    y_pred = clf.predict(X_test)
+    y_pred_proba = clf.predict_proba(X_test)[:,1]
+    return y_pred, y_pred_proba
+
+def metrics(y_test, y_pred, y_pred_proba):
+
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    auc = roc_auc_score(y_test, y_pred_proba)
+    metrics_dict={
+    'accuracy': accuracy, 
+    'precision': precision,
+    'recall': recall, 
+    'auc': auc}
+    return metrics_dict
+
+def save_metrics(path,metrics_dict):
+    with open(path, 'w') as f:
+        json.dump(metrics_dict, f, indent=4)
+
+def main():
+    clf=load_model('emotion-detection/models/model.pkl')
+    X_test, y_test=load_data('./emotion-detection/data/processed/test_bow.csv')
+    y_pred, y_pred_proba=make_prediction(clf,X_test)
+    metrics_dict=metrics(y_test, y_pred, y_pred_proba)
+    save_metrics('emotion-detection/reports/metrics.json', metrics_dict)
+
+
+
+if __name__=='__main__':
+    main()
